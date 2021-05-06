607d4db07c2f597f728618c155b81e3bca8c3a2b
lewist13 <lewistpro@gmail.com>
2021-05-06 12:31:55 -0400

    built foundation for backend server file

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
 const express = require('express');
+const app = express();
+const cors = require('cors');
+const logger = require('morgan');
+const AppRouter = require('./routes/AppRouter');
+const http  = require('http').createServer.app;
+
+const PORT = process.env.PORT || 3001
+
+app.use(express.static(path.join(__dirname, 'client', 'build')))
+app.use(logger('dev'))
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.use('/api', AppRouter)
+app.get('*', (req, res) => 
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+)
+http.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`))
